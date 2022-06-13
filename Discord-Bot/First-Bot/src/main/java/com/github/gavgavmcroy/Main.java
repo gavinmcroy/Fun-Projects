e85710e6a29d64bba2f8cb5b6c6639fafb9a341a
Gav <41348589+gavgavmcroy@users.noreply.github.com>
2022-06-12 21:03:31 -0400

    Basic Bot

--- Discord-Bot/First-Bot/src/main/java/com/github/gavgavmcroy/Main.java
+++ Discord-Bot/First-Bot/src/main/java/com/github/gavgavmcroy/Main.java
@@ -1,46 +1,101 @@
 package com.github.gavgavmcroy;
 
-import discord4j.core.DiscordClient;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import com.sedmelluq.discord.lavaplayer.track.playback.NonAllocatingAudioFrameBuffer;
+import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
-import discord4j.core.object.entity.Message;
-import reactor.core.publisher.Mono;
+import discord4j.core.object.VoiceState;
+import discord4j.core.object.entity.Member;
+import discord4j.core.object.entity.channel.VoiceChannel;
+import discord4j.voice.AudioProvider;
 
-import java.io.File;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    /* Token needs to come from config file instead of hard coded*/
+    private static final Map<String, Command> commands = new HashMap<>();
+
     public static void main(String[] args) throws Exception {
-        File file = new File("C:\\Users\\Gavin T McRoy\\Documents\\GitHub\\Fun-Projects\\" +
-                "Discord-Bot\\First-Bot\\info.cfg");
-        Scanner scanner = new Scanner(file);
+        GatewayDiscordClient client = DiscordClientBuilder.create(Util.openToken()).build().login().block();
 
-        String localToken = scanner.nextLine();
+        /* This is our message listener */
+        Objects.requireNonNull(client).getEventDispatcher().on(MessageCreateEvent.class).subscribe(event -> {
+            final String content = event.getMessage().getContent();
+            for (Map.Entry<String, Command> entry : commands.entrySet()) {
+                if (content.startsWith("!" + entry.getKey())) {
+                    entry.getValue().execute(event);
+                    break;
+                }
+            }
+        });
 
-        DiscordClient client = DiscordClient.create(localToken);
+        client.onDisconnect().block();
+    }
 
-        Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) ->
-                gateway.on(MessageCreateEvent.class, event -> {
-                    Message message = event.getMessage();
+    static {
+        /* Grab the member, if not there return null */
+        /* Creates AudioPlayer instance and translates URLS to AudioTrack instances */
+        final AudioPlayerManager playerManager = new DefaultAudioPlayerManager();
 
-                    if (message.getContent().equalsIgnoreCase("!dick")) {
-                        Random random = new Random();
-                        int randomNum = random.nextInt(50);
-                        String size = "\u0190";
-                        for (int i = 0; i < randomNum; i++) {
-                            size = size.concat("=");
-                        }
-                        size = size.concat("D");
-                        String finalSize = size;
-                        return message.getChannel()
-                                .flatMap(channel -> channel.createMessage(finalSize));
+        /* Optimization Strategy */
+        playerManager.getConfiguration().setFrameBufferFactory(NonAllocatingAudioFrameBuffer::new);
+
+        /* Allow playerManager to parse remote sources like youtube */
+        AudioSourceManagers.registerRemoteSources(playerManager);
+
+        /* Create an AudioPlayer so Discord4J can receive audio data */
+        final AudioPlayer player = playerManager.createPlayer();
+        AudioProvider provider = new LavaPlayerAudioProvider(player);
+
+        commands.put("ping", event -> Objects.requireNonNull(event.getMessage().getChannel().block()).
+                createMessage("Pong").block());
+
+        commands.put("dick", event -> {
+            Random random = new Random();
+            int randomNum = random.nextInt(50);
+            String size = "\u0190";
+            for (int i = 0; i < randomNum; i++) {
+                size = size.concat("=");
+            }
+            size = size.concat("D");
+            String finalSize = size;
+            Objects.requireNonNull(event.getMessage().getChannel().block()).createMessage(finalSize).block();
+        });
+
+        commands.put("flip", event -> {
+            Random random = new Random();
+            int randomNum = random.nextInt(2);
+            String string = event.getMember().get().getMention();
+            //String string = " ";
+            if (randomNum == 0) {
+                Objects.requireNonNull(event.getMessage().getChannel().block()).createMessage(string + " Heads").block();
+            } else {
+                Objects.requireNonNull(event.getMessage().getChannel().block()).createMessage(string + " Tails").block();
+            }
+        });
+
+        commands.put("join", event -> {
+            Member member = event.getMember().orElse(null);
+            if (member != null) {
+                VoiceState voiceState = member.getVoiceState().block();
+                if (voiceState != null) {
+                    VoiceChannel channel = voiceState.getChannel().block();
+                    if (channel != null) {
+                        channel.join(spec -> spec.setProvider(provider)).block();
                     }
+                }
+            }
+        });
 
-                    return Mono.empty();
-                }));
+        commands.put("play", event -> {
+            final TrackScheduler scheduler = new TrackScheduler(player);
+            final String content = event.getMessage().getContent();
+            final List<String> command = Arrays.asList(content.split(" "));
+            playerManager.loadItem(command.get(1), scheduler);
 
-        login.block();
+        });
     }
 }
---
+++ Discord-Bot/First-Bot/src/main/java/com/github/gavgavmcroy/TrackScheduler.java
@@ -0,0 +1,36 @@
+package com.github.gavgavmcroy;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
+import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+
+public class TrackScheduler implements AudioLoadResultHandler {
+
+    private AudioPlayer player;
+
+    public TrackScheduler(final AudioPlayer player) {
+        this.player = player;
+    }
+
+    @Override
+    public void trackLoaded(AudioTrack track) {
+        player.playTrack(track);
+    }
+
+    @Override
+    public void playlistLoaded(AudioPlaylist playlist) {
+
+    }
+
+    @Override
+    public void noMatches() {
+
+    }
+
+    @Override
+    public void loadFailed(FriendlyException exception) {
+
+    }
+}
