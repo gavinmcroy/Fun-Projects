dc3f89c37642a570964d7ed659bafcfb2d412f4f
Gav <41348589+gavgavmcroy@users.noreply.github.com>
2022-06-14 18:32:29 -0400

    more methods to implement eventually

--- Discord-Bot/First-Bot/src/main/java/com/github/gavgavmcroy/TrackScheduler.java
+++ Discord-Bot/First-Bot/src/main/java/com/github/gavgavmcroy/TrackScheduler.java
@@ -2,11 +2,13 @@ package com.github.gavgavmcroy;
 
 import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter;
 import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
 import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
 
-public class TrackScheduler implements AudioLoadResultHandler {
+public class TrackScheduler extends AudioEventAdapter implements AudioLoadResultHandler {
 
     private final AudioPlayer player;
     private final TrackState trackState;
@@ -37,6 +39,45 @@ public class TrackScheduler implements AudioLoadResultHandler {
         trackState.loadFailed();
     }
 
+    @Override
+    public void onPlayerPause(AudioPlayer player) {
+        /* TODO */
+    }
+
+    @Override
+    public void onPlayerResume(AudioPlayer player) {
+        /* TODO */
+    }
+
+    @Override
+    public void onTrackStart(AudioPlayer player, AudioTrack track) {
+        /* TODO */
+    }
+
+    @Override
+    public void onTrackEnd(AudioPlayer player, AudioTrack track, AudioTrackEndReason endReason) {
+        if (endReason.mayStartNext) {
+            /* TODO */
+        }
+
+        // endReason == FINISHED: A track finished or died by an exception (mayStartNext = true).
+        // endReason == LOAD_FAILED: Loading of a track failed (mayStartNext = true).
+        // endReason == STOPPED: The player was stopped.
+        // endReason == REPLACED: Another track started playing while this had not finished
+        // endReason == CLEANUP: Player hasn't been queried for a while, if you want you can put a
+        //                       clone of this back to your queue
+    }
+
+    @Override
+    public void onTrackException(AudioPlayer player, AudioTrack track, FriendlyException exception) {
+        /* TODO */
+    }
+
+    @Override
+    public void onTrackStuck(AudioPlayer player, AudioTrack track, long thresholdMs) {
+        /* TODO */
+    }
+
     /* Once a state has been grabbed, the track will no longer remain in that state. For example
      *  If you want to check if a track loaded, and it failed, handle it. The state wouldn't need to remain
      *  in an inoperable state. The class checking the state should not have to reset it. It is assumed once a state is
@@ -48,5 +89,6 @@ public class TrackScheduler implements AudioLoadResultHandler {
     public void clearStatus() {
         trackState.clearStatus();
     }
+
 
 }
