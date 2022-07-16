a03fcc01b2acb6e53be9a0e520fccae741b4119a
Gav <41348589+gavgavmcroy@users.noreply.github.com>
2022-07-16 17:31:38 -0400

    github copilot is insane

--- Interview-Preparation/Programming Skills-1/Sum of All Odd Length Subarray/main.cpp
+++ Interview-Preparation/Programming Skills-1/Sum of All Odd Length Subarray/main.cpp
@@ -1,6 +1,56 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+// print data in reverse order
+void print_reverse(std::string data)
+{
+    for (int i = data.length() - 1; i >= 0; i--)
+    {
+        std::cout << data[i];
+    }
+}
+
+// read in file and print contents
+void read_file(std::string file_name)
+{
+    std::ifstream file;
+    file.open(file_name);
+    std::string line;
+    while (getline(file, line))
+    {
+        std::cout << line << std::endl;
+    }
+    file.close();
+}
+
+// find the average of a vector of numbers
+double average(std::vector<double> data)
+{
+    double sum = 0;
+    for (int i = 0; i < data.size(); i++)
+    {
+        sum += data[i];
+    }
+    return sum / data.size();
+}
+
+// add up all values in a vector
+int sum_vector(std::vector<int> data)
+{
+    int sum = 0;
+    for (int i = 0; i < data.size(); i++)
+    {
+        sum += data[i];
+    }
+    return sum;
+}
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
