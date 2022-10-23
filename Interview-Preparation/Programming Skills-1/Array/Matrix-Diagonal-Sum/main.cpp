820fbea6fad9878dc3d22aacef1b97b1c166c88e
Gav <41348589+gavgavmcroy@users.noreply.github.com>
2022-10-23 11:35:28 -0400

    with old solution

---
+++ Interview-Preparation/Programming Skills-1/Array/Matrix-Diagonal-Sum/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int diagonalSum(vector<vector<int>> &mat);
+
+int main() {
+    vector<vector<int>> matrix = {{1, 1, 1, 1},
+                                  {1, 1, 1, 1},
+                                  {1, 1, 1, 1},
+                                  {1, 1, 1, 1}};
+    std::cout << diagonalSum(matrix);
+
+    return 0;
+}
+
+int diagonalSum(vector<vector<int>> &mat) {
+    int sum = 0;
+    int farRight = mat.size() - 1;
+    for (int i = 0; i < mat.size(); i++) {
+        /* secondary diagonal (top right to bottom left)*/
+        if (i != farRight) {
+            sum += mat[i][farRight];
+        }
+        farRight--;
+        sum += mat[i][i];
+//        for(int j = 0; j < mat[i].size(); j++){
+//            //primary diagonal
+//            if(i==j){
+//                sum+=mat[i][j];
+//            }
+//        }
+    }
+
+    /* Its square so the elements in the column is the same as rows */
+    //int farRight = mat.size()-1;
+//    for(int i = 0; i < mat.size(); i++){
+//        if(i!=farRight){
+//            sum+=mat[i][farRight];
+//        }
+//        farRight--;
+//    }
+
+    return sum;
+}
