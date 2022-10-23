94458bf26bf2be53b09d2f31626a54fbdc9f2d7c
Gav <41348589+gavgavmcroy@users.noreply.github.com>
2022-10-23 11:37:27 -0400

    linear solution

--- Interview-Preparation/Programming Skills-1/Array/Matrix-Diagonal-Sum/main.cpp
+++ Interview-Preparation/Programming Skills-1/Array/Matrix-Diagonal-Sum/main.cpp
@@ -23,24 +23,26 @@ int diagonalSum(vector<vector<int>> &mat) {
         if (i != farRight) {
             sum += mat[i][farRight];
         }
-        farRight--;
+        /* primary diagonal */
         sum += mat[i][i];
+        farRight--;
+    }
+    return sum;
+}
+
+
 //        for(int j = 0; j < mat[i].size(); j++){
 //            //primary diagonal
 //            if(i==j){
 //                sum+=mat[i][j];
 //            }
 //        }
-    }
 
-    /* Its square so the elements in the column is the same as rows */
-    //int farRight = mat.size()-1;
+/* Its square so the elements in the column is the same as rows */
+//int farRight = mat.size()-1;
 //    for(int i = 0; i < mat.size(); i++){
 //        if(i!=farRight){
 //            sum+=mat[i][farRight];
 //        }
 //        farRight--;
 //    }
-
-    return sum;
-}
