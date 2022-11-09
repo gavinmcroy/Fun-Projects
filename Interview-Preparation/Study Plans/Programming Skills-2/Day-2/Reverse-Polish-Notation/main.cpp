35d399e474cf19bccfec01b415f3f618b47dbb07
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2022-11-09 11:08:51 -0500

    psuedo code seems correct

--- Interview-Preparation/Study Plans/Programming Skills-2/Day-2/Reverse-Polish-Notation/main.cpp
+++ Interview-Preparation/Study Plans/Programming Skills-2/Day-2/Reverse-Polish-Notation/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <string>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+int evalRPN(vector<string> &tokens);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<string> data = {"11", "+", "-"};
     return 0;
+}
+
+int evalRPN(vector<string> &tokens) {
+    /* General algorithm
+     * Push numbers until we run into an operator
+     * if operator pop 2 numbers off, apply operation, push them back to stack
+     * Repeat until stack is empty */
+    
+    for (int i = 0; i < tokens.size(); i++) {
+        if (tokens[i] == "+") {
+
+        } else if (tokens[i] == "-") {
+
+        } else if (tokens[i] == "*") {
+
+        } else if (tokens[i] == "/") {
+
+        } else {
+
+        }
+    }
 }
