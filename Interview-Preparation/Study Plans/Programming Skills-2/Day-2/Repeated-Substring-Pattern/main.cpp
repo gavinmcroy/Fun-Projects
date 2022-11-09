9bbef2d01575f26c07934ced0d7094f327355cf4
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2022-11-09 08:40:04 -0500

    horrendous solution

--- Interview-Preparation/Study Plans/Programming Skills-2/Day-2/Repeated-Substring-Pattern/main.cpp
+++ Interview-Preparation/Study Plans/Programming Skills-2/Day-2/Repeated-Substring-Pattern/main.cpp
@@ -7,18 +7,21 @@ bool repeatedSubstringPattern(string s);
 
 string largestSubString(string s);
 
+bool smallestSubString(string s);
+
 int main() {
     string value = "abaabaa";
     string value2 = "abababab";
     string value3 = "ababababab";
-    string value4 = "aabaaba";
-    std::cout << largestSubString(value3);
+    string value4 = "aabaab";
+    //std::cout << largestSubString(value3);
+    std::cout << repeatedSubstringPattern(value);
+    std::cout << smallestSubString(value);
     //std::cout << value.substr(0,2);
     return 0;
 }
 
 bool repeatedSubstringPattern(string s) {
-    string possibleSol;
     if (s.size() < 2) {
         return false;
     }
@@ -35,34 +38,50 @@ bool repeatedSubstringPattern(string s) {
         return true;
     }
 
-    possibleSol = largestSubString(s);
+    string possibleSol;
+    int startingSize = 2;
+    for (int i = 2; i < s.size(); i++) {
+        int increment = 0;
+        /* if at least half the string has yet to yield a solution then there is no substring */
+        if (startingSize > (s.size() / 2) + 1) {
+            break;
+        }
+        string sub = s.substr(0, startingSize);
+
+        /* Compare our substring at 0, for each letter, if one letter differs, unsuccessful match, exit sub loop */
+        for (int j = i; j < s.size(); j++) {
+            /* invalid solution */
+            if (sub[increment] != s[j]) {
+                break;
+            }
+            increment++;
+
+            /* Solution found */
+            if (increment >= sub.size()) {
+                /* See if we can construct a string with the given possible substring */
+                possibleSol = sub;
+                string test;
+                int amount = s.size() / possibleSol.size();
+                for (int i = 0; i < amount; i++) {
+                    test += possibleSol;
+                }
+                if (test == s) {
+                    return true;
+                }
+                break;
+            }
+        }
+        startingSize++;
+    }
+
     if (possibleSol.empty()) {
         return false;
     }
 
-
-    /* breaks on abaabaa*/
-    std::cout<<possibleSol<<std::endl;
-
-    /* Take our possible substr and see if it can be appended to create the start string letter by letter */
-    string check;
-    int incr = 0;
-    for(int i = 0; i < s.size(); i++ ){
-        if(incr >= possibleSol.size()){
-            incr = 0;
-        }
-        check += possibleSol[incr];
-        incr++;
-    }
-
-    if (s != check) {
-        return false;
-    }
-
-    return true;
+    return false;
 }
 
-string largestSubString(string s){
+string largestSubString(string s) {
     /* Our substring must be at least 2 letters, so start with the first two letters */
     string possibleSol;
     int startingSize = 2;
@@ -90,6 +109,45 @@ string largestSubString(string s){
         }
         startingSize++;
     }
-    std::cout<<possibleSol<<std::endl;
+    std::cout << possibleSol << std::endl;
     return possibleSol;
+}
+
+bool smallestSubString(string s) {
+    /* Our substring must be at least 2 letters, so start with the first two letters */
+    string possibleSol;
+    int startingSize = 2;
+    for (int i = 2; i < s.size(); i++) {
+        int increment = 0;
+        /* if at least half the string has yet to yield a solution then there is no substring */
+        if (startingSize > (s.size() / 2) + 1) {
+            break;
+        }
+        string sub = s.substr(0, startingSize);
+
+        /* Compare our substring at 0, for each letter, if one letter differs, unsuccessful match, exit sub loop */
+        for (int j = i; j < s.size(); j++) {
+            /* invalid solution */
+            if (sub[increment] != s[j]) {
+                break;
+            }
+            increment++;
+
+            /* Solution found */
+            if (increment >= sub.size()) {
+                possibleSol = sub;
+                string test;
+                int amount = s.size() / possibleSol.size();
+                for (int i = 0; i < amount; i++) {
+                    test += possibleSol;
+                }
+                if (test == s) {
+                    return true;
+                }
+                break;
+            }
+        }
+        startingSize++;
+    }
+    return false;
 }
