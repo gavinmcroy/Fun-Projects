cc6b2a82de1ee77262e1762fa2b45a32f97f67c4
Gav <41348589+gavgavmcroy@users.noreply.github.com>
2022-11-08 21:13:10 -0500

    almost solved

--- Interview-Preparation/Study Plans/Programming Skills-2/Day-2/Repeated-Substring-Pattern/main.cpp
+++ Interview-Preparation/Study Plans/Programming Skills-2/Day-2/Repeated-Substring-Pattern/main.cpp
@@ -7,7 +7,7 @@ bool repeatedSubstringPattern(string s);
 
 int main() {
     string value = "abab";
-    std::cout << repeatedSubstringPattern("aabaab");
+    std::cout << repeatedSubstringPattern("abaabaa");
     //std::cout << value.substr(0,2);
     return 0;
 }
@@ -17,18 +17,67 @@ bool repeatedSubstringPattern(string s) {
         return false;
     }
 
-    /* the first letter of the string must repeat, so once it repeats, take the substring */
-    char reference = s[0];
-    string temp;
-    for (int i = 1; i < s.size(); i++) {
-        /* this could be a potential substr */
-        if (reference == s[i]) {
-            temp = s.substr(0, i);
-            break;
+    /* Test if all letters are the same */
+    char temp = s[0];
+    bool allSame = true;
+    for (char x: s) {
+        if (x != temp) {
+            allSame = false;
         }
     }
-    std::cout << temp;
+    if (allSame) {
+        return true;
+    }
 
-    return false;
+    /* Our substring must be at least 2 letters, so start with the first two letters */
+    string possibleSol;
+    int startingSize = 2;
+    for (int i = 2; i < s.size(); i++) {
+        int increment = 0;
+        /* if at least half the string has yet to yield a solution then there is no substring */
+        if (startingSize > (s.size() / 2) + 1) {
+            break;
+        }
+        string sub = s.substr(0, startingSize);
 
+        /* Compare our substring at 0, for each letter, if one letter differs, unsuccessful match, exit sub loop */
+        for (int j = i; j < s.size(); j++) {
+            /* invalid solution */
+            if (sub[increment] != s[j]) {
+                break;
+            }
+            increment++;
+
+            /* Solution found */
+            if (increment >= sub.size()) {
+                possibleSol = sub;
+                break;
+            }
+        }
+        startingSize++;
+    }
+
+    if (possibleSol.empty()) {
+        return false;
+    }
+
+
+    /* breaks on abaabaa*/
+
+    /* Take our possible substr and see if it can be appended to create the start string letter by letter */
+    string check;
+    int incr = 0;
+    for(int i = 0; i < s.size(); i++ ){
+        if(incr >= possibleSol.size()){
+            incr = 0;
+        }
+        check += possibleSol[incr];
+        incr++;
+    }
+
+    if (s != check) {
+        return false;
+    }
+
+    return true;
 }
