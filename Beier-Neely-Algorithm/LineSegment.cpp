f961140d4f7da0b053596ba61c3eff3d2cce51c4
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2022-12-06 21:46:31 -0500

    dope

---
+++ Beier-Neely-Algorithm/LineSegment.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Gavin Taylor Mcroy on 12/2/22.
+//
+
+#include "LineSegment.h"
+
+std::fstream LineSegment::loadSegments(const std::string& fileName) {
+    std::fstream file;
+    return file;
+}
--- Beier-Neely-Algorithm/main.cpp
+++ Beier-Neely-Algorithm/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <OpenImageIO/imageio.h>
 
-int main() {
+/* input morph, output morph, txt with directed line segments */
+int main(int args, char * argv[]) {
+
+    /* Load morph1 into memory */
+    /* Load morph2 into memory */
+
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
