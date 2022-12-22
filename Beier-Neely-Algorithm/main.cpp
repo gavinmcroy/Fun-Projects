fc196a13711ff05491a94fdbf2de0ca7e2b79eb4
Gav <41348589+gavgavmcroy@users.noreply.github.com>
2022-12-22 13:53:03 -0500

    basic image Input

--- Beier-Neely-Algorithm/main.cpp
+++ Beier-Neely-Algorithm/main.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 #include <OpenImageIO/imageio.h>
+#include <vector>
 
 using namespace OIIO;
-using namespace OpenImageIO_v2_4;
 
 /* input morph, output morph, txt with directed line segments */
-int main(int args, char * argv[]) {
-    std::string filename = "file.png";
-    std::string file2;
-
-    /* Load morph1 into memory */
+int main(int args, char *argv[]) {
+    std::string filename = "input.jpeg";
     auto input = ImageInput::open(filename);
+
+    if (!input) {
+        std::cerr << "Error loading image" << std::endl;
+        exit(1);
+    }
+    const ImageSpec &spec = input->spec();
+    int xRes = spec.width;
+    int yRes = spec.height;
+    int channels = spec.nchannels;
+    std::vector<unsigned char> pixels(xRes * yRes * channels);
+
+    input->read_image(TypeDesc::UINT8, &pixels[0]);
+    input->close();
 
 
     std::cout << "Hello, World!" << std::endl;
