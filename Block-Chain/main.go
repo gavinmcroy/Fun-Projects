1864950ea3632f6267ecbf542bb2c3040d0d4f36
Gav <41348589+gavgavmcroy@users.noreply.github.com>
2023-01-05 17:09:58 -0500

    simple block chain

--- Block-Chain/main.go
+++ Block-Chain/main.go
@@ -1,9 +1,62 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"time"
 )
+
+var BlockChain []Block
+
+type Block struct {
+	Index     int
+	TimeStamp string
+	BPM       int
+	Hash      string
+	PrevHash  string
+}
 
 func main() {
 	fmt.Println("Lets see if go works")
+}
+
+func calculateHash(block Block) string {
+	record := string(block.Index) + block.TimeStamp + string(block.BPM) + block.PrevHash
+	h := sha256.New()
+	h.Write([]byte(record))
+	hashed := h.Sum(nil)
+	return hex.EncodeToString(hashed)
+}
+
+func generateBlock(oldBlock Block, BPM int) (Block, error) {
+	var newBlock Block
+	t := time.Now()
+	newBlock.Index = oldBlock.Index + 1
+	newBlock.TimeStamp = t.String()
+	newBlock.BPM = BPM
+	newBlock.PrevHash = oldBlock.Hash
+	newBlock.Hash = calculateHash(newBlock)
+
+	return newBlock, nil
+}
+
+func isBlockValid(newBlock Block, oldBlock Block) bool {
+	if oldBlock.Index+1 != newBlock.Index {
+		return false
+	}
+	if oldBlock.Hash != newBlock.PrevHash {
+		return false
+	}
+	if calculateHash(newBlock) != newBlock.Hash {
+		return false
+	}
+
+	return true
+}
+
+func replaceChain(newBlocks []Block) {
+	if len(newBlocks) > len(BlockChain) {
+		BlockChain = newBlocks
+	}
 }
