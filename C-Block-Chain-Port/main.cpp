6313a9e432a910913b2978668da089dd282f9a27
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2023-01-10 10:54:09 -0500

    link update

--- C-Block-Chain-Port/main.cpp
+++ C-Block-Chain-Port/main.cpp
@@ -2,6 +2,8 @@
 
 #include "SHA256.h"
 
+//implemented based on https://blog.boot.dev/cryptography/how-sha-2-works-step-by-step-sha-256/#how-does-the-sha-256-algorithm-work
+
 int main() {
     SHA256 sha256;
     std::string val = "hello world";
