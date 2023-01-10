2e9c301da33bd83db92c7a9e0c29f1c7bcce240e
Gavin Mcroy (Mac) <gavin.taylor.mcroy@gmail.com>
2023-01-10 12:31:24 -0500

    refactoring and round constants

--- C-Block-Chain-Port/SHA256.cpp
+++ C-Block-Chain-Port/SHA256.cpp
@@ -5,6 +5,7 @@
 #include "SHA256.h"
 
 #include <utility>
+#include <vector>
 
 std::string SHA256::convertDigitToBinary(unsigned int digit) {
     std::string binary;
@@ -32,7 +33,10 @@ std::string SHA256::convertStringToBinary(std::string &input) {
 }
 
 std::string SHA256::sha256(std::string &input) {
-    /* fractional parts of the square roots of the first 8 primes */
+    /* useful constants */
+    const int MULTIPLE = 512;
+    const int STRING_BIT_SIZE = 64;
+    /* hash value: fractional parts of the square roots of the first 8 primes */
     const int H0 = 0x6a09e667;
     const int H1 = 0xbb67ae85;
     const int H2 = 0x3c6ef372;
@@ -42,22 +46,43 @@ std::string SHA256::sha256(std::string &input) {
     const int H6 = 0x1f83d9ab;
     const int H7 = 0x5be0cd19;
 
+    /* first 32 bits of the fractional parts of the cube root of the first 2-311 prime */
+    std::vector<unsigned int> roundConstants = {0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1,
+                                                0x923f82a4,
+                                                0xab1c5ed5,
+                                                0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe,
+                                                0x9bdc06a7, 0xc19bf174,
+                                                0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa,
+                                                0x5cb0a9dc, 0x76f988da,
+                                                0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147,
+                                                0x06ca6351, 0x14292967,
+                                                0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb,
+                                                0x81c2c92e, 0x92722c85,
+                                                0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624,
+                                                0xf40e3585, 0x106aa070,
+                                                0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a,
+                                                0x5b9cca4f, 0x682e6ff3,
+                                                0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb,
+                                                0xbef9a3f7, 0xc67178f2};
+
     /* Pre-processing */
+    std::string zeros;
     std::string hash = convertStringToBinary(input);
     std::string originalSizeInBinary = convertDigitToBinary(hash.size());
     hash += "1";
-    int padding = 512 - 64;
-    /* data must a multiple of 512 so append 0's to allow it to be
-     * Save 64 bits for integer at the end which is length of binary string */
-    while (hash.size() % padding != 0) {
+
+    /* Save 64 bits for integer at the end which is length of binary string */
+    const int PADDING = MULTIPLE - STRING_BIT_SIZE;
+    /* data must a multiple of 512 so append 0's to allow it to be */
+    while (hash.size() % PADDING != 0) {
         hash += "0";
     }
-    std::string zeros;
+
+    /* fill empty space with 0's until there is 64 bits */
     for (int i = originalSizeInBinary.size(); i < 64; i++) {
         zeros.append("0");
     }
     zeros += originalSizeInBinary;
-
 
     if (zeros.size() != 64) {
         std::cerr << "Fatal error: in converting size to binary ";
@@ -65,9 +90,11 @@ std::string SHA256::sha256(std::string &input) {
     if ((zeros.size() + hash.size()) % 512 != 0) {
         std::cerr << "Fatal error: hash is not a multiple of 512";
     }
-
-
     hash += zeros;
+
+    
+
+
 
 
     return " ";
